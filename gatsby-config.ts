5f802beff9f8cddcfef9bb365b58394440c0b7e7
Jatindra Suthar <jatindra.suthar@ibm.com>
2023-10-27 12:50:37 +0530

    changes

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,6 +5,7 @@ const config: GatsbyConfig = {
     title: `sample-gatsby`,
     siteUrl: `https://www.yourdomain.tld`
   },
+  pathPrefix: "/sample-gatsy",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
